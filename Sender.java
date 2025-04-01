7f59378768afa133f45bea864abd371b22e3754d
Rupam <rupammondal564@gmail.com>
2025-04-01 08:21:35 +0530

    Update

---
+++ Sender.java
@@ -0,0 +1,34 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+public class Sender {
+
+    public Sender() throws Exception{
+        DatagramSocket socket = new DatagramSocket();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter msg = ");
+        String msg = sc.nextLine();
+
+        byte[] buffer = msg.getBytes();
+
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length , InetAddress.getByName("127.0.0.1") , 3000);
+        socket.send(packet);
+
+        buffer = new byte[1500];
+        packet = new DatagramPacket(buffer, buffer.length);
+        socket.receive(packet);
+
+        String msg1 = new String(buffer).trim();
+        System.out.println(msg1);
+    }
+    public static void main(String[] args) {
+        try {
+            new Sender();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
